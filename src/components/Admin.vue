336f0b3a4af5eb581689d9df8b3a2c746369f42b
Zhang Zhihao <zhzhang.4390@gmail.com>
2020-03-25 08:05:20 +0800

    Bug fix: admin bottom border

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="py-10 border-b-2 flex">
-    <ul class="py-10 px-5 w-64 bg-gray-300">
-      <router-link
-        to="/admin/users"
-        class="block p-4 border-b border-gray-400 text-lg font-semibold"
-      >Users</router-link>
-      <router-link
-        to="/admin/products"
-        class="block p-4 border-b border-gray-400 text-lg font-semibold"
-      >Products</router-link>
-      <router-link
-        to="/admin/requests"
-        class="block p-4 border-b border-gray-400 text-lg font-semibold"
-      >Requests</router-link>
-    </ul>
+  <div>
+    <div class="py-10 border-b-2 border-gray-400 flex">
+      <ul class="py-10 px-5 w-64 bg-gray-300">
+        <router-link
+          to="/admin/users"
+          class="block p-4 border-b border-gray-400 text-lg font-semibold"
+        >Users</router-link>
+        <router-link
+          to="/admin/products"
+          class="block p-4 border-b border-gray-400 text-lg font-semibold"
+        >Products</router-link>
+        <router-link
+          to="/admin/requests"
+          class="block p-4 border-b border-gray-400 text-lg font-semibold"
+        >Requests</router-link>
+      </ul>
 
-    <div class="p-10 w-full bg-gray-100">
-      <router-view></router-view>
+      <div class="p-10 w-full bg-gray-100">
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
